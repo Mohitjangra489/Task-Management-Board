baec838edcc2e8ea9efca20a5e241cb66b20c4c1
Mohitjangra489 <119482895+Mohitjangra489@users.noreply.github.com>
2023-10-02 13:37:17 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,99 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState} from 'react';
+import Homepage from './components/Homepage';
 
- const arr=[
-    {
-      state:"Gujarat",
-      city:{
-        city1:"Bhuj",
-        city2:"Dwarka"
-      }
-    },
-    {
-      state:"Goa",
-      city:{
-        city1:"Panji",
-        city2:"Madgaon"
-      }
-    },
-    {
-      state:"Ladakh",
-      city:{
-        city1:"Barwani",
-        city2:"Betul"
-      }
-    },
-    {
-      state:"Haryana",
-      city:{
-        city1:"Bhiwani",
-        city2:"hansi"
-      }
-    },
-    {
-      state:"Chhattisgarh",
-      city:{
-        city1:"Raipur",
-        city2:"Bilaspur"
-      }
-    },
-  ];
+import Mainpage from './components/Mainpage';
+
+
 
 function App() {
-//  console.log(arr);
-  const[data,setdata]=useState(arr);
-  const[stat,setstat]=useState("Gujarat");
-  const[city1,setcity1]=useState("Bhuj");
-  const[city2,setcity2]=useState("Dwarka");
 
-  let handleselect=function(val){
-    console.log("inside");
-    console.log(val);
-    setstat(val);
-    let filteritem=arr.filter((item)=>{
-      console.log(item);
-      if(item.state==val)
-      {
-      return(item) ;
-      }
-    })
-    console.log(filteritem);
-    setcity1(filteritem[0].city.city1);
-    setcity2(filteritem[0].city.city2); 
-
-  }
-
-  useEffect(()=>{
- console.log("inside useeffect",city1,city2);
-  },[city1,city2]);
-
-
-  return (
-    <div className="App">
-     <h1>React practice</h1>
-    <div>
-    <select name='state' onChange={(e)=>{handleselect(e.target.value)}}>
-      {
-        data.map((item,i)=>{
-          return(
-              <option key={i}>{item.state}</option>
-          )
-        })
-      }a
-        </select>
-    </div>
-    <div>
-      <select name='city'>
-        <option>{city1}</option>
-        <option>{city2}</option>
-      </select>
-    </div>
-    <div>
-      <h1> Selected State is: {stat} and Cities in {stat} are:{city1} and {city2}</h1>
-    </div>
-    </div>
-  );
+  return(
+    <>
+    <h1>hello</h1>
+    <Mainpage/>
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+ 
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-it2dupms2hc1pa32.us.auth0.com"
+    clientId="BPrDFbhdiAQPIRafUQkVGq9mM0jE8Qkq"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <App />
+    </Auth0Provider>,
   </React.StrictMode>
 );
 
